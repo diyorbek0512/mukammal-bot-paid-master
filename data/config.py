162001871478019f3269df487ec1086a50396734
diyorbek0512 <bahodirovdiyorbek2007@gmail.com>
2024-02-04 16:32:45 +0500

    just

--- data/config.py
+++ data/config.py
@@ -1,4 +1,4 @@
-from environs import Env
+''' from environs import Env
 
 # environs kutubxonasidan foydalanish
 env = Env()
@@ -7,5 +7,12 @@ env.read_env()
 # .env fayl ichidan quyidagilarni o'qiymiz
 BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
 ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-IP = env.str("ip")  # Xosting ip manzili
+IP = env.str("ip")  # Xosting ip manzili '''
+
+import os
+
+BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))
+ADMINS = list(os.environ.get("ADMINS"))
+IP = str(os.environ.get("ip"))
+
 
